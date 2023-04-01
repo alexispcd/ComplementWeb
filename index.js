2f81ef8fee1aea2a67e6af64d18830b2b3c39ff6
alexispcd <95770334+alexispcd@users.noreply.github.com>
2023-04-01 19:12:41 +0200

    COUNTRIES V4 | FILTERS

--- index.js
+++ index.js
@@ -23,15 +23,10 @@ class Country {
     }
 
     toString() {
-        if (this.borders == []) {
-            this.borders = "pas de frontières";
-        }
-        return "Nom: " + this.name.fr + "\n Code :" + this.alpha3Code + "\n Population: " + this.population + "\n Superficie: " + this.area + "\n Densité: " + this.getPopDensity() + "\n Capitale: " + this.capital + "\n Région: " + this.region + "\n Démographie: " + this.demonym +  "\n Devise: " + this.getCurrencies() +  "\n Frontières: " + this.borders + "\n";
-        
+        return this.name;
     }
 
     getPopDensity() {
-        //check if result in NaN
         return this.area === 0 ? 0 : this.population / this.area;
     }
 
@@ -107,31 +102,83 @@ window.onload = async function () {
 
         // HTML CREATION
         let htmlCountry = document.createElement("tr");
+        htmlCountry.dataset.language = country.languages.join(",");
+        htmlCountry.dataset.name = country.name.fr;
+        htmlCountry.dataset.region = country.region;
         htmlCountry.id = country.alpha3Code;
         htmlCountry.className = "country";
-        countryNameString = "'" + String(country.alpha3Code) + "'";
-        flagLink = "'" + String(country.flag) + "'";
         htmlCountry.innerHTML =
             `
-            <td onclick="afficherDiv(${countryNameString})">${country.name.fr}</td>
-            <td onclick="afficherDiv(${countryNameString})">${country.population}</td>
-            <td onclick="afficherDiv(${countryNameString})">${country.area}</td>
-            <td onclick="afficherDiv(${countryNameString})">${country.getPopDensity()}</td>
-            <td onclick="afficherDiv(${countryNameString})">${country.region}</td>
-            <td onclick="afficheDrapeau(${flagLink})">
-                <img src="${country.flag}"/>
-            </td>
+            <td>${country.name.fr}</td>
+            <td>${country.population}</td>
+            <td>${country.area}</td>
+            <td>${country.getPopDensity()}</td>
+            <td>${country.region}</td>
+            <td>
+                <img src="${country.flag}" alt="${country.name.fr} flag"/>
+            </td> 
             `
         ;
         elemCountries[0].appendChild(htmlCountry);
     }
 
-    pagination();
+    let regionFilter = document.getElementById('region')
+    for (let country of Object.values(Country.all_countries)) {
+        if (!regionFilter.innerHTML.includes(country.region)) {
+            regionFilter.innerHTML += `<option value="${country.region}">${country.region}</option>`
+        }
+    }
+
+    let languageFilter = document.getElementById('language')
+    for (let language of Object.values(Language.all_languages)) {
+        languageFilter.innerHTML += `<option value="${language.iso639_2}">${language.name}</option>`
+    }
+
+    let nameFilter = document.getElementById('name')
+
+    regionFilter.addEventListener("change", applyFilters);
+    languageFilter.addEventListener("change", applyFilters);
+    nameFilter.addEventListener("input", applyFilters);
+
+    pagination("all", "all", "")
+
+    function applyFilters() {
+        let region = regionFilter.value;
+        let language = languageFilter.value;
+        let name = nameFilter.value;
+        pagination(region, language, name);
+    }
 }
 
-function pagination() {
+function filter(countries, region, language, name) {
+    let filteredCountries = [];
+
+    for (let i = 0; i < countries.length; i++) {
+        let country = countries[i];
+
+        if (region !== "all" && country.dataset.region !== region) {
+            continue;
+        }
+
+        if (language !== "all" && !country.dataset.language.includes(language)) {
+            continue;
+        }
+
+        if (name !== "" && !country.dataset.name.includes(name)) {
+            continue;
+        }
+
+        filteredCountries.push(country);
+    }
+
+    return filteredCountries;
+}
+
+function pagination(region, language, name) {
     let countries = document.getElementsByClassName("country");
-    let nbPages = Math.ceil(countries.length / 25);
+    let filteredCountries = filter(countries, region, language, name);
+
+    let nbPages = Math.ceil(filteredCountries.length / 25);
     let page = 1;
 
     let htmlTotalPages = document.getElementById("totalPages");
@@ -140,141 +187,36 @@ function pagination() {
     let htmlPage = document.getElementById("page");
     htmlPage.innerHTML = page;
 
-    for (let i = 0; i < countries.length; i++) {
-        if (i >= 25) {
-            countries[i].style.display = "none";
-        }
-    }
+    paginationButton();
 
     let previous = document.getElementById("previous");
     let next = document.getElementById("next");
 
-    previous.addEventListener("click", function() {
+    previous.onclick = function () {
         if (page > 1) {
             page--;
-            paginationButton()
+            paginationButton();
         }
-    });
+    }
 
-    next.addEventListener("click", function() {
+    next.onclick = function () {
         if (page < nbPages) {
             page++;
-            paginationButton()
+            paginationButton();
         }
-    });
+    }
 
     function paginationButton() {
-        htmlPage.innerHTML = page;
+        let countries = document.getElementsByClassName("country");
+
+        let start = (page - 1) * 25;
+        let end = start + 25;
+        let pageCountries = filteredCountries.slice(start, end);
+
         for (let i = 0; i < countries.length; i++) {
-            if (i >= (page - 1) * 25 && i < page * 25) {
-                countries[i].style.display = "";
-            } else {
-                countries[i].style.display = "none";
-            }
-
+            !pageCountries.includes(countries[i]) ? countries[i].style.display = "none" : countries[i].style.display = ""
         }
+
+        htmlPage.innerHTML = page;
     }
 }
-function afficheDrapeau(drapeau) {
-        // Créé un élément HTML pour l'image
-        var img = document.createElement("img");
-        img.src = drapeau;
-        img.style.position = "fixed";
-        img.style.top = "50%";
-        img.style.left = "50%";
-        img.style.transform = "translate(-50%, -50%)";
-        img.style.zIndex = "9999";
-        img.style.width = "50%";
-        img.style.height = "auto";
-        
-        // Ajoute l'image à la page
-        document.body.appendChild(img);
-        
-        // Créé un élément HTML pour le fond sombre
-        var fondSombre = document.createElement("div");
-        fondSombre.style.position = "fixed";
-        fondSombre.style.top = "0";
-        fondSombre.style.left = "0";
-        fondSombre.style.width = "100%";
-        fondSombre.style.height = "100%";
-        fondSombre.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-        fondSombre.style.zIndex = "9998";
-        img.style.transform = "translate(-50%, -50%) scale(0)";
-        img.style.transition = "transform 0.5s, opacity 0.5s";
-        
-        // Ajoute le fond sombre à la page
-        document.body.appendChild(fondSombre);
-        
-        setTimeout(function() {
-            img.style.transform = "translate(-50%, -50%) scale(1)";
-            img.style.opacity = "1";
-          }, 50);
-
-        // Ajoute un événement pour cacher l'image lorsque l'on clique en dehors
-        fondSombre.addEventListener("click", function() {
-          document.body.removeChild(img);
-          document.body.removeChild(fondSombre);
-        });
-}
-
-function getcountrybyalpha3code(alpha3code) {
-    for (let country of Object.values(Country.all_countries)) {
-        if (country.alpha3Code === alpha3code) {
-            return country;
-        }
-    }
-}
-
-function parseStringToHtmlList(inputString) {
-    const lines = inputString.split('\n'); // sépare la chaîne en plusieurs lignes
-    let result = '<ul id="animation">';
-    for (let i = 0; i < lines.length; i++) {
-      const line = lines[i].trim(); // retire les espaces en début et fin de ligne
-      if (line.length > 0) {
-        const [type, content] = line.split(':'); // sépare la ligne en type et contenu
-        result += `<li><strong>${type.trim()}:</strong> ${content.trim()}</li>`; // ajoute le type et le contenu à la liste HTML
-      }
-    }
-    result += "</ul>";
-    return result;
-  }
-
-function afficherDiv(countryName) {
-
-    var maDiv = document.createElement("div");
-    maDiv.style.position = "fixed";
-    maDiv.style.top = "50%";
-    maDiv.style.left = "50%";
-    maDiv.style.transform = "translate(-50%, -50%)";
-    maDiv.style.zIndex = "9999";
-    maDiv.style.backgroundColor = "white";
-    maDiv.style.padding = "20px";
-    maDiv.style.frontSize = "18em";
-    
-    actualCountry = getcountrybyalpha3code(countryName);
-    maDiv.innerHTML = `${parseStringToHtmlList(actualCountry.toString())} `;
-
-    // Ajouter la div à la page
-    document.body.appendChild(maDiv);
-    
-    var fondSombre = document.createElement("div");
-    fondSombre.style.position = "fixed";
-    fondSombre.style.top = "0";
-    fondSombre.style.left = "0";
-    fondSombre.style.width = "100%";
-    fondSombre.style.height = "100%";
-    fondSombre.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-    fondSombre.style.zIndex = "9998";
-    
-    // Ajouter le fond sombre
-    document.body.appendChild(fondSombre);
-    
-
-    // Cache la div 
-    fondSombre.addEventListener("click", function() {
-      document.body.removeChild(maDiv);
-      document.body.removeChild(fondSombre);
-    });
-    
-    return maDiv;
-  }
